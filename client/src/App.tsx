191a81211099e826f64071e0dfad52c83da530fa
Sorokina-Alina <alina9551@icloud.com>
2024-03-14 15:18:15 +0300

    server-config

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h2>hello</h2>
+    </div>
+  );
+}
+
+export default App;
